4428a30f84ef70f39a804a4e7c4bf62ebe1b7d46
Shiran Pasternak <shiranpasternak@gmail.com>
2013-05-16 13:41:30 -0400

    Small example wrapping an Ensembl API call.

--- services/gramene-api.js
+++ services/gramene-api.js
@@ -1,12 +1,39 @@
+var http    = require("http");
 var Service = require('../sage/src/service.js');
+var ensemblConf = require("../conf/ensembl.json");
 
 var service = new Service();
 
+function ensemblGET(path, callback) {
+    http.get({
+        host: ensemblConf.host,
+        port: ensemblConf.port,
+        path: path,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }, function (response) {
+        response.setEncoding('utf8');
+        response.on('data', callback);
+    });
+}
+
 service.get('/genome/:genomeId', function (req, res, next) {
-    // TODO: Return genome resource.
-    res.send({
-        message: "SOS"
+    ensemblGET('/assembly/info/' + req.params.genomeId,
+    function (json) {
+        var assembly = JSON.parse(json);
+        var genome = { chromosomes: [] };
+        assembly.top_level_seq_region_names.forEach(function (chr) {
+            genome.chromosomes.push({
+                name: chr,
+                uri: "http://api.gramene.org/genome/" +
+                    req.params.genomeId +
+                    "/chromosome/" + chr
+            });
+        });
+        res.send(genome);
     });
 });
 
+// TODO: Have caller start/stop service.
 service.start({ port: 4747 });
