1a9eb18aa5d748ed97c6ba73a73c09029de795ce
Ken Youens-Clark <kyclark@gmail.com>
2013-10-30 10:43:49 -0600

    Setting up Mojo branch.

---
+++ lib/Grm/API/Search.pm
@@ -0,0 +1,41 @@
+package Grm::API::Search;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use lib '/usr/local/gramene-lib/lib';
+use Grm::Search;
+use Grm::Utils qw( camel_case commify iterative_search_values timer_calc );
+
+# This action will render a template
+sub search {
+    my $self     = shift;
+#    my $search   = Grm::Search->new;
+#    my $timer  = timer_calc();
+#    my $req      = $self->req;
+#    my $qry      = $req->param('qry')      or die 'No query defined';
+#    my $format   = $req->param('fmt')      || 'json';
+#    my $db       = $req->param('db')       || '';
+#    my $category = $req->param('category') || '';
+#    my $taxonomy = $req->param('taxonomy') || '';
+#
+#    my $res;
+#    for my $t ( iterative_search_values( $qry ) ) {
+#        $res         =  $search->search_mysql(
+#            query    => $t, 
+#            category => $category,
+#            taxonomy => $taxonomy,
+#            db       => $db,
+#        );
+#
+#        last if $res->{'num_hits'} > 0;
+#    }
+#
+#    $res->{'time'} = $timer->();
+
+    $self->respond_to(
+        json => { response => 'hi' }, #$res,
+        html => sub { $self->render },
+    );
+}
+
+1;
