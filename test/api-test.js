55f404f3f1ebce43e0e1350ff0e74e1663f4cd43
Shiran Pasternak <shiranpasternak@gmail.com>
2013-10-30 21:07:16 -0400

    Fixed test.

--- test/api-test.js
+++ test/api-test.js
@@ -8,16 +8,30 @@ describe("API", function () {
         API.should.be.an.Object;
     });
     it("should include top-level resources", function (done) {
+        Sage.Registry.listen(4444);
         API.listen(12345);
-        supertest(API).get("/").end(function (err, res) {
-            [err].should.be.null;
-            res.body.should.eql({
-                resources: {
-                    "http://0.0.0.0:12345/gene": {},
-                    "http://0.0.0.0:12345/genome": {}
-                }
-            });
-            done();
+        Sage.Registry.add({
+            name: "ensembl",
+            url: "http://0.0.0.0:11111"
         });
+        Sage.Registry.add(API);
+
+        // FIXME: should use events, not timeouts
+        setTimeout(function () {
+            supertest(API).get("/").end(function (err, res) {
+                [err].should.be.null;
+                res.body.should.eql({
+                    resources: {
+                        "http://0.0.0.0:12345/gene": {
+                            name: { type: "string" }
+                        },
+                        "http://0.0.0.0:12345/genome": {
+                            name: { type: "string", required: true }
+                        }
+                    }
+                });
+                done();
+            });
+        }, 200);
     });
 });
